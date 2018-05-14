8a1954d5680b4460e4b74cb7a7e4ebb663e51a94
Freddy Chen <cccsen526@gmail.com>
2018-05-14 17:35:07 +0800

    First file of this app

---
+++ mkbrain.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/line/line-bot-sdk-go/linebot"
+)
+
+func main() {
+	bot, err := linebot.New(
+		os.Getenv("CHANNEL_SECRET"),
+		os.Getenv("CHANNEL_TOKEN"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
