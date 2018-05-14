0bbf02e3789c5b8544dd4eaafa0b63dedcd668be
Freddy Chen <cccsen526@gmail.com>
2018-05-14 18:15:44 +0800

    simple POC

--- mkbrain.go
+++ mkbrain.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/line/line-bot-sdk-go/linebot"
@@ -15,4 +16,32 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	http.HandleFunc("/callback", func(w http.ResponseWriter, req *http.Request) {
+		events, err := bot.ParseRequest(req) //events is []*Event
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, event := range events { //event is *Event
+			if event.Type == linebot.EventTypeMessage { //"message" should be defined in /callback
+				switch message := event.Message.(type) {
+				case *linebot.TextMessage:
+					if message.Text == "恰特猴" {
+						message.Text = "幹嘛~?"
+					} else if message.Text == "本週+1" {
+						profile, err := bot.GetGroupMemberProfile(event.Source.GroupID, event.Source.UserID).Do()
+						if err != nil {
+							log.Print(err)
+						}
+						message.Text = "好喔, 本週 " + profile.DisplayName + " +1, 吱吱"
+					}
+					_, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do()
+					if err != nil {
+						log.Print(err)
+					}
+				}
+
+			}
+		}
+	})
 }
