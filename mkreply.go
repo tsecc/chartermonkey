597189268b1a6b67b1242c75c6585283e3deba80
Freddy Chen <cccsen526@gmail.com>
2018-05-19 23:40:20 +0800

    fix err

--- mkreply.go
+++ mkreply.go
@@ -13,20 +13,20 @@ func reply(message string, event *linebot.Event, bot *linebot.Client) (reply str
 	if message == "恰特猴" {
 		reply = "幹嘛~?"
 	} else if message == "list" {
-		message = mknote.Query()
+		reply = mknote.Query()
 	} else if message == "+1" && event.Source.GroupID != "" {
 		profile, err := bot.GetGroupMemberProfile(event.Source.GroupID, event.Source.UserID).Do()
 		if err != nil {
 			log.Print(err)
 		}
-		message = "好喔, " + profile.DisplayName + " +1, 吱吱"
+		reply = "好喔, " + profile.DisplayName + " +1, 吱吱"
 	} else if message == "+1" && event.Source.GroupID == "" {
 		profile, err := bot.GetProfile(event.Source.UserID).Do()
 		if err != nil {
 			log.Print(err)
 		}
 		date := time.Now().Local().Format("2014-07-07")
-		message = "好喔, 今天是" + date + ", 下次 " + profile.DisplayName + " +1, 吱吱"
+		reply = "好喔, 今天是" + date + ", 下次 " + profile.DisplayName + " +1, 吱吱"
 	}
 
 	return reply
