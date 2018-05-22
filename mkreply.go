6446b71064efac9caea173ed346b5505092ab983
Freddy Chen <cccsen526@gmail.com>
2018-05-22 23:22:06 +0800

    reduce useless comments

--- mkreply.go
+++ mkreply.go
@@ -13,7 +13,7 @@ import (
 func reply(message string, event *linebot.Event, bot *linebot.Client) (reply string) {
 	switch message {
 	case "恰特猴":
-		reply = "幹嘛~?"
+		reply = "幹嘛~?" //need to use tmpl
 	case "list":
 		reply = mknote.Query()
 	case "+1":
@@ -36,51 +36,11 @@ func reply(message string, event *linebot.Event, bot *linebot.Client) (reply str
 			log.Print(reply)
 		} else {
 			//personal add, shouldn't happen...only for admin.
-			reply = "建議不要私底下揪團~吱吱"
+			reply = "建議不要私底下揪團~吱吱" //need to use tmpl
 		}
 	}
 
 	return reply
-
-	// if message == "恰特猴" {
-	// 	reply = "幹嘛~?"
-	// } else if message == "list" {
-	// 	reply = mknote.Query()
-	// } else if message == "+1" && event.Source.GroupID != "" {
-	// 	profile, err := bot.GetGroupMemberProfile(event.Source.GroupID, event.Source.UserID).Do()
-	// 	if err != nil {
-	// 		log.Print(err)
-	// 	}
-	// 	resultBool := mknote.Add(profile.DisplayName) //returned a in64 1=added, 0=failed
-	// 	tmpl, err := template.ParseFiles("message.tpl")
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-
-	// 	tmplname := "failed" //default to failed if SQL update has failed
-	// 	if resultBool == 1 {
-	// 		tmplname = "plusone"
-	// 	}
-
-	// 	var bytedata bytes.Buffer
-	// 	tmpl.ExecuteTemplate(&bytedata, tmplname, profile.DisplayName)
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// 	reply = bytedata.String()
-	// } else if message == "+1" && event.Source.GroupID == "" {
-	// 	profile, err := bot.GetProfile(event.Source.UserID).Do()
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// 	result := mknote.Add(profile.DisplayName)
-	// 	log.Printf("%d", result)
-	// 	//date := time.Now().Local().Format("2014-07-07")
-	// 	reply = "好喔, " + profile.DisplayName + " +1, 吱吱"
-	// } else {
-	// 	reply = "吱吱, 我聽不懂哦"
-	// }
-
 }
 
 func assembleReply(tplID string, myProfile Profile) string {
